f87e9c21acff0d8841bd3cec5a46779036cbb200
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2020-08-15 18:35:53 -0300

    Mobile - Proffy

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,45 @@
+import React, { ReactNode } from 'react';
+import { View, Image, Text } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler'
+
+import logoImg from '../../assets/images/logo.png'
+import backIcon from '../../assets/images/icons/back.png'
+import { useNavigation } from '@react-navigation/native';
+
+import styles from './styles';
+
+interface IPageHeaderProps {
+  title: string
+  headerRight?: ReactNode
+}
+
+const PageHeader: React.FC<IPageHeaderProps> = ({ title, headerRight, children }) => {
+  const { navigate } = useNavigation()
+  
+  function handleGoBack(){
+    navigate('Landing')
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <BorderlessButton 
+          onPress={handleGoBack}
+        >
+          <Image source={backIcon} resizeMode="contain" />
+        </BorderlessButton>
+
+        <Image source={logoImg} resizeMode="contain" />
+      </View>
+
+      <View style={styles.header}>
+        <Text style={styles.title}>{title}</Text>
+        {headerRight}
+      </View>
+
+      {children}
+    </View>
+  )
+}
+
+export default PageHeader;
